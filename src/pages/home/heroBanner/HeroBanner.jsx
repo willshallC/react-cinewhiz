8c01c16e946fa1b22a638df00b0285031211ccb5
willshallC <netcreaterss@gmail.com>
2024-02-01 11:48:31 +0530

    setup navigate for search query

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
 import "./style.scss";
 
 const HeroBanner = () => {
+    const [backgroundm, setBackground] = useState("");
+    const [query, setQuery] = useState("");
+    
+    const navigate = useNavigate();
+    const searchQueryHandler = (event) =>{
+        setQuery('gg');
+        if(event.key === "Enter" && query.length > 0 ){
+            navigate(`/search/${query}`)
+        }
+    }
     return(
-        <div>
+        <div className="heroBanner">
+            <div className="wrapper">
+                <div className="heroBannerContent">
+                    <span className="title">Welcome</span>
+                    <span className="subTitle">Millions of movies , TV shows and people to discover.
+                        Explore Now.
+                    </span>
+                    <div className="searchInput">
+                        <input onChange={e=>setQuery(e.target.value)} onKeyUp={searchQueryHandler} type="text" placeholder="Search for a movie or TV show....."/>
+                        <button>Search</button>
+                    </div>
+                </div>
+            </div>
             Hero Banner
         </div>
     )
